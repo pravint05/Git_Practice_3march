d42b143d397c60dae49c581ce30e60724db61557
Tellstar <Tellstar@DESKTOP-B6C79S4>
2023-03-05 12:05:28 +0530

    changes by p005

--- src/practice/Demo1.java
+++ src/practice/Demo1.java
@@ -3,9 +3,7 @@ package practice;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, Currently i'm working on Git");
-		System.out.println("Today date:3/3/23  and day: Friday");
-		System.out.println("Make changes with feature/pravin");
+		int i=10;
 
 	}
 
